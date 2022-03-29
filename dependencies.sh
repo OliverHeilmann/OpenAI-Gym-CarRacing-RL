7faf1c0f898fd53726ef6302888e4576abc172ae
OliverHeilmann <oheilmann5@gmail.com>
2022-03-29 11:41:03 +0100

    first commit

---
+++ dependencies.sh
@@ -0,0 +1,6 @@
+# To install dependencies, run the following in terminal (Mac and Linux):
+# --> sh dependencies.sh
+
+conda install -c conda-forge gym
+conda install -c conda-forge pyglet
+conda install -c cogsci pygame
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Code Description
+WIP
+
+# Contributors
+1) Oliver Heilmann: __contact details__
+2) ...
+3) ..
+
+# Notes
+1) To select interpreter --> cmd+shift+p, Python. Select Interpreter, CHOOSE PREFERENCE
