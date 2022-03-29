36408215761b96bd72e74591b0eca3500588b2e4
Sam Barba <sambarba99@gmail.com>
2022-03-29 13:30:07 +0100

    Revert joe's stupid change

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import time
 
 bool_do_not_quit = True  # Boolean to quit pyglet
 scores = []  # Your gaming score
-a = np.array( [0.0, 0.0, 0.0] )  # Actions joe
+a = np.array( [0.0, 0.0, 0.0] )  # Actions
 
 def key_press(k, mod):
     global bool_do_not_quit, a, restart
