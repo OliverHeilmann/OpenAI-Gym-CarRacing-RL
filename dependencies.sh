e3f639585b70a3efeeffa3eebc74fed7f717ee20
OliverHeilmann <oheilmann5@gmail.com>
2022-03-31 15:38:48 +0100

    Update dependencies list to include stuff for Deep QN and OpenCV

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,10 @@
 # To install dependencies, run the following in terminal (Mac and Linux):
+# --> conda activate <the environment you want to use e.g. base>
 # --> sh dependencies.sh
 
 conda install -c conda-forge gym
 conda install -c conda-forge pyglet
 conda install -c cogsci pygame
 conda install -c conda-forge box2d-py
+conda install -c conda-forge opencv
+conda install -c conda-forge tensorflow
