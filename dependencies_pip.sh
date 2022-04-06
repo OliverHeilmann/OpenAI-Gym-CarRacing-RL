7a7c8faecab06103661b1d6116d17265a0ee4e95
OliverHeilmann <oheilmann5@gmail.com>
2022-04-06 12:08:35 +0100

    update dependencies to include pip version

---
+++ dependencies_pip.sh
@@ -0,0 +1,12 @@
+# To install dependencies, run the following in terminal (Mac and Linux):
+# --> conda activate <the environment you want to use e.g. base>
+# --> sh dependencies_pip.sh
+# --> see screen usage (for running on servers): https://linuxize.com/post/how-to-use-linux-screen/
+
+pip install gym
+pip install pyglet
+pip install pygame
+pip install box2d-py
+pip install opencv-python
+pip install tensorflow
+pip install screen
