cb9ea1f16718d6a07587bc8293603dde6c8dfa8e
Oliver <49697817+OliverHeilmann@users.noreply.github.com>
2022-04-11 20:57:14 +0100

    Update dependencies_conda.sh

--- dependencies_conda.sh
+++ dependencies_conda.sh
@@ -2,9 +2,10 @@
 # --> conda activate <the environment you want to use e.g. base>
 # --> sh dependencies.sh
 
+conda install -c conda-forge tensorflow
+conda install -c conda-forge keras
+conda install -c conda-forge opencv
 conda install -c conda-forge gym
 conda install -c conda-forge pyglet
 conda install -c cogsci pygame
 conda install -c conda-forge box2d-py
-conda install -c conda-forge opencv
-conda install -c conda-forge tensorflow
