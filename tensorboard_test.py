018665ef3108cc9d176419d7b1ca838d1ae32a7c
OliverHeilmann <oheilmann5@gmail.com>
2022-04-13 13:53:57 +0100

    update on gpu memory

--- tensorboard_test.py
+++ tensorboard_test.py
@@ -6,8 +6,8 @@ import os
 # From: https://stackoverflow.com/questions/34199233/how-to-prevent-tensorflow-from-allocating-the-totality-of-a-gpu-memory
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
-    # tf.config.experimental.set_memory_growth(gpu, True)   # set memory growth option
-    tf.config.set_logical_device_configuration( gpu, [tf.config.LogicalDeviceConfiguration(memory_limit=3000)] )    # set memory limit to 3 GB
+    tf.config.experimental.set_memory_growth(gpu, True)   # set memory growth option
+    # tf.config.set_logical_device_configuration( gpu, [tf.config.LogicalDeviceConfiguration(memory_limit=3000)] )    # set memory limit to 3 GB
 
 
 mnist = tf.keras.datasets.mnist
