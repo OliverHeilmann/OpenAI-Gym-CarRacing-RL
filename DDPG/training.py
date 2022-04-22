b2056aab7421c08a9b5e1bc45d2ce0e0be2f5ded
hadi-jalali <58006557+hadi-jalali@users.noreply.github.com>
2022-04-22 14:07:17 +0100

    functioning ddpg
    run training to train (shocking I know), you can stop it from running before finishing all episodes. it saves the best model everytime it's discovered but to get a graph all episodes have to run. training won't show you anything special but running validation shows the car trying to stay on track

---
+++ DDPG/training.py
@@ -0,0 +1,86 @@
+"""
+Main Loop of simulation.
+Here a problem and a solution are defined.
+"""
+
+import gym
+from pyglet.window import key
+import numpy as np
+
+from utils import *
+from agent_ddpg import *
+
+
+# Show preview
+def key_press(k, mod):
+    if k == key.SPACE:
+        global preview
+        preview = True
+
+
+def key_release(k, mod):
+    if k == key.SPACE:
+        global preview
+        preview = False
+
+
+# Parameters
+num_episodes = 1000
+
+gym.logger.set_level(40)
+preview = False
+best_result = 0
+all_episode_reward = []
+
+# Initialize simulation
+env = gym.make('CarRacing-v0')
+env.reset()
+env.viewer.window.on_key_press = key_press
+env.viewer.window.on_key_release = key_release
+
+# Define custom standard deviation for noise
+noise_std = np.array([0.1, 4 * 0.2], dtype=np.float32)
+agent = AgentDDPG(env.action_space, model_outputs=2, noise_std=noise_std)
+
+# Loop of episodes
+for ep in range(num_episodes):
+    state = env.reset()
+    agent.reset()
+    done = False
+    episode_reward = 0
+    no_reward_counter = 0
+
+    # One-step-loop
+    while not done:
+        if preview:
+            env.render()
+
+        action, train_action = agent.get_action(state)
+
+        # This will make steering much easier
+        action /= 4
+        new_state, reward, done, info = env.step(action)
+
+        # Models action output has a different shape for this problem
+        agent.learn(state, train_action, reward, new_state)
+        state = new_state
+        episode_reward += reward
+
+        if reward < 0:
+            no_reward_counter += 1
+            if no_reward_counter > 200:
+                break
+        else:
+            no_reward_counter = 0
+
+    all_episode_reward.append(episode_reward)
+    average_result = np.array(all_episode_reward[-100:]).mean()
+    print('Episode ', ep, ' result:', episode_reward, '..last 100 Average results:', average_result)
+
+    if episode_reward > best_result:
+        print('Saving this model because it is the best one so far')
+        agent.save_solution()
+        best_result = episode_reward
+
+episode_indices = [i+1 for i in range(num_episodes)]
+plot_learning_curve(episode_indices, all_episode_reward, 'ddpg.png')
--- ddpg/replay_buffer.py
+++ ddpg/replay_buffer.py
@@ -1,38 +1,49 @@
 import numpy as np
+from utils import *
 
 
 class ReplayBuffer:
-    def __init__(self, max_size, input_shape, number_actions):
-        self.mem_size = max_size
-        self.mem_counter = 0
-        self.state_memory = np.zeros((self.mem_size, *input_shape))
-        self.new_state_memory = np.zeros((self.mem_size, *input_shape))
-        self.action_memory = np.zeros((self.mem_size, number_actions))
-        self.reward_memory = np.zeros(self.mem_size)
-        self.flag_memory = np.zeros(self.mem_size, dtype=np.bool)
+    def __init__(self, mem_size=50000):
+        self.mem_size = mem_size
 
-    def save_move(self, state, action, reward, new_state, done):
-        idx = self.mem_counter % self.mem_size
-        self.state_memory[idx] = state
-        self.new_state_memory[idx] = new_state
-        self.action_memory[idx] = action
-        self.reward_memory[idx] = reward
-        self.flag_memory[idx] = done
-        self.mem_counter += 1
+        # Memory will be initialized when first time used
+        self.state_db = None
+        self.action_db = None
+        self.reward_db = None
+        self.new_state_db = None
 
-    def smaple_buffer(self, batch_size):
-        # how much of memory is filled up (ignoring 0s)
-        max_mem = min(self.mem_counter, self.mem_size)
+        self.writes_num = 0
 
-        batch = np.random.choice(max_mem, batch_size, replace=False)
+    def init_memory(self, state_shape, action_shape):
+        state_shape = prepend_tuple(self.mem_size, state_shape)
+        action_shape = prepend_tuple(self.mem_size, action_shape)
 
-        states = self.state_memory[batch]
-        new_states = self.new_state_memory[batch]
-        rewards = self.reward_memory[batch]
-        flags = self.flag_memory[batch]
-        actions = self.action_memory[batch]
+        self.state_db = np.zeros(state_shape, np.float32)
+        self.action_db = np.zeros(action_shape, np.float32)
+        self.reward_db = np.zeros((self.mem_size, 1), np.float32)
+        self.new_state_db = np.zeros(state_shape, np.float32)
 
-        return states, new_states, rewards, flags, actions
+    def save_move(self, state, action, reward, new_state):
+        if self.state_db is None:
+            self.init_memory(state.shape, action.shape)
 
+        # Write indexes
+        memory_index = self.writes_num % self.mem_size
+        next_index = (self.writes_num + 1) % self.mem_size
 
+        # Save next state to the same array with a next index
+        self.state_db[memory_index] = state
+        self.action_db[memory_index] = action
+        self.reward_db[memory_index] = reward
+        self.new_state_db[memory_index] = new_state
 
+        self.writes_num += 1
+
+    def sample_buffer(self, batch_size=64):
+        indexes_range = min(self.mem_size, self.writes_num)
+        sampled_indexes = np.random.choice(indexes_range, batch_size)
+
+        return (self.state_db[sampled_indexes],
+                self.action_db[sampled_indexes],
+                self.reward_db[sampled_indexes],
+                self.new_state_db[sampled_indexes])
